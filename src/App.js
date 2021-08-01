da588b545c35ef465f0f9dfe0381eb5f8b04e1d3
Daniel Fullman <djdane101@gmail.com>
2021-08-01 15:20:39 -0400

    adding google tracking tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import ReactGA from 'react-ga';
+
 import './App.css';
 
 import HeroContainer from './components/hero-container';
@@ -6,6 +8,14 @@ import Projects from './components/projects';
 import About from './components/about';
 import Contact from './components/contact';
 import Footer from './components/footer';
+
+const trackingId = "281370654"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
+
 function App() {
   return (
     <div className="App">
